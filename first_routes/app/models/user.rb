59af79ccfba300f3cdc8c020329ed9b9f8a0703d
Will Bannister <bannister.will@gmail.com>
2022-11-10 12:00:03 -0800

    completed model class builds

--- first_routes/app/models/user.rb
+++ first_routes/app/models/user.rb
@@ -3,14 +3,19 @@
 # Table name: users
 #
 #  id         :bigint           not null, primary key
-#  name       :string           not null
-#  email      :string           not null
+#  username   :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :username, presence: true
+
+  has_many :artworks, foreign_key: :artist_id, dependent: :destroy
+  has_many :artwork_shares, foreign_key: :viewer_id, dependent: :destroy
+
+  has_many :shared_artworks,
+    through: :artwork_shares,
+    source: :artwork
 
 
 end
--- first_routes/db/seeds.rb
+++ first_routes/db/seeds.rb
@@ -10,24 +10,41 @@
 ApplicationRecord.transaction do
   # Destroy tables (not necessary if using `rails db:seed:replant`)
   puts 'Destroying tables...'
+  ArtworkShare.destroy_all
+  Artwork.destroy_all
   User.destroy_all
+
 
   # Reset primary keys (i.e., ids)
   puts 'Resetting id sequences...'
-  %w(users).each do |table_name|
+  %w(users artwork_shares artworks).each do |table_name|
     ApplicationRecord.connection.reset_pk_sequence!(table_name)
   end
 
   # Create seed data
   puts 'Creating seed data...'
-  u1 = User.create!(name: 'noam zimet', email: 'nz@gmail.com')
-  u2 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
-  u3 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
-  u4 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
-  u5 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
-  u6 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
-  u7 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
-  u8 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
+
+  u1 = User.create!(username: 'noam zimet')
+  u2 = User.create!(username: 'will bannister')
+  u3 = User.create!(username: 'alvin')
+  u4 = User.create!(username: 'alexander')
+  u5 = User.create!(username: 'bobby')
+  u6 = User.create!(username: 'dave')
+  u7 = User.create!(username: 'lucy')
+  u8 = User.create!(username: 'anna')
+
+
+  a1 = Artwork.create!(title: 'mona lisa', image_url: 'google.com', artist_id: u1.id)
+  a2 = Artwork.create!(title: 'the persistence of time', image_url: 'google2.com', artist_id: u2.id)
+  a3 = Artwork.create!(title: 'american gothic', image_url: 'googl3e.com', artist_id: u1.id)
+  a4 = Artwork.create!(title: 'the scream', image_url: 'goog4le.com', artist_id: u4.id)
+
+
+  ArtworkShare.create!(artwork_id: a1.id, viewer_id: u2.id) # shares mona lisa with will
+  ArtworkShare.create!(artwork_id: a4.id, viewer_id: u1.id) # shares the scream with noam
+
+
+
 
   puts 'Done!'
 end
