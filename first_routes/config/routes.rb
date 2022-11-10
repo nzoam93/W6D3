00cd1302d2c09a1e90855df9aa9994b9e1ad3186
Noam Z <nzoam93@gmail.com>
2022-11-09 16:02:05 -0800

    added users resources

--- first_routes/config/routes.rb
+++ first_routes/config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users
+  # resources :users,  except: [:new, :edit]
+
+  # get 'users', to: 'users#index', as: 'users'
+  # get 'users/:id', to: 'users#show', as: 'user'
+  # post 'users', to: 'users#create'
+  # patch 'users/:id', to: 'users#update'
+  # put 'users/:id', to: 'users#update'
+  # delete 'users/:id', to: 'users#destroy'
+  # get 'users/new', to: 'users#new', as: 'new_user'
+  # get 'users/edit', to: 'users#edit', as: 'edit_user'
+
+
+
+
 end
