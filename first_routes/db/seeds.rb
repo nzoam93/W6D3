c1ed5d3a4c1317eb1a5db05d0356eafb6d8ecd4b
Will Bannister <bannister.will@gmail.com>
2022-11-09 16:55:09 -0800

    created seed

--- first_routes/db/seeds.rb
+++ first_routes/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+ApplicationRecord.transaction do
+  # Destroy tables (not necessary if using `rails db:seed:replant`)
+  puts 'Destroying tables...'
+  User.destroy_all
+
+  # Reset primary keys (i.e., ids)
+  puts 'Resetting id sequences...'
+  %w(users).each do |table_name|
+    ApplicationRecord.connection.reset_pk_sequence!(table_name)
+  end
+
+  # Create seed data
+  puts 'Creating seed data...'
+  u1 = User.create!(name: 'noam zimet', email: 'nz@gmail.com')
+  u2 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
+  u3 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
+  u4 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
+  u5 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
+  u6 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
+  u7 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
+  u8 = User.create!(name: 'will bannister', email: 'wb@gmail.com')
+
+  puts 'Done!'
+end
